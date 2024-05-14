617047d72b20e0a04b462694d5479dc3c9c53eed
Mike <mike.zhang.619@gmail.com>
2024-05-14 15:00:13 +0200

    no_grad when forwarding clip text encoder

--- util/util.py
+++ util/util.py
@@ -25,6 +25,7 @@ def extract_clip_feature(labelset, model_name="ViT-B/32"):
     # "ViT-L/14@336px" # the big model that OpenSeg uses
     print("Loading CLIP {} model...".format(model_name))
     clip_pretrained, _ = clip.load(model_name, device='cuda', jit=False)
+    clip_pretrained.eval()
     print("Finish loading")
 
     if isinstance(labelset, str):
@@ -40,7 +41,8 @@ def extract_clip_feature(labelset, model_name="ViT-B/32"):
         labels.append(label)
     text = clip.tokenize(labels)
     text = text.cuda()
-    text_features = clip_pretrained.encode_text(text)
+    with torch.no_grad():
+        text_features = clip_pretrained.encode_text(text)
     text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
     return text_features
