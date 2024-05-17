d7404c844ee18d326620a9c7b72c23a1451c1a48
Mike <mike.zhang.619@gmail.com>
2024-05-17 15:37:12 +0200

    map labels to a single text prompt string

--- comparison_label_mappings.py
+++ comparison_label_mappings.py
@@ -1,30 +1,30 @@
-HABITAT_OGN_LABEL_TO_TEXT_PROMPT = {
+HABITAT_OGN_LABELS_TO_TEXT_PROMPTS = {
     # 21 habitat object goal nav categories along with 'other' category
     # Map each raw label to a "gramatically correct" text prompt to be encoded
     # by the text embedding model
-    "chair": ["a chair in a scene"],
-    "table": ["a table in a scene"],
-    "picture": ["a picture in a scene"],
-    "cabinet": ["a cabinet in a scene"],
-    "cushion": ["a cushion in a scene"],
-    "sofa": ["a sofa in a scene"],
-    "bed": ["a bed in a scene"],
-    "chest_of_drawers": ["a chest of drawers in a scene"],
-    "plant": ["a plant in a scene"],
-    "sink": ["a sink in a scene"],
-    "toilet": ["a toilet in a scene"],
-    "stool": ["a stool in a scene"],
-    "towel": ["a towel in a scene"],
-    "tv_monitor": ["a television monitor in a scene"],
-    "shower": ["a shower in a scene"],
-    "bathtub": ["a bathtub in a scene"],
-    "counter": ["a counter in a scene"],
-    "fireplace": ["a fireplace in a scene"],
-    "gym_equipment": ["gym equipment in a scene"],
-    "seating": ["seating in a scene"],
-    "clothes": ["clothes in a scene"],
+    "chair": "a chair in a scene",
+    "table": "a table in a scene",
+    "picture": "a picture in a scene",
+    "cabinet": "a cabinet in a scene",
+    "cushion": "a cushion in a scene",
+    "sofa": "a sofa in a scene",
+    "bed": "a bed in a scene",
+    "chest_of_drawers": "a chest of drawers in a scene",
+    "plant": "a plant in a scene",
+    "sink": "a sink in a scene",
+    "toilet": "a toilet in a scene",
+    "stool": "a stool in a scene",
+    "towel": "a towel in a scene",
+    "tv_monitor": "a television monitor in a scene",
+    "shower": "a shower in a scene",
+    "bathtub": "a bathtub in a scene",
+    "counter": "a counter in a scene",
+    "fireplace": "a fireplace in a scene",
+    "gym_equipment": "gym equipment in a scene",
+    "seating": "seating in a scene",
+    "clothes": "clothes in a scene",
 
-    "other": ["other in a scene"],
+    "other": "other in a scene",
 }
 
 HABITAT_OGN_LABELS = (
@@ -55,38 +55,38 @@ HABITAT_OGN_LABELS = (
 
 
 
-MATTERPORT_REGION_LABELS_TO_TEXT_PROMPT = {
+MATTERPORT_REGION_LABELS_TO_TEXT_PROMPTS = {
     # Matterport regions labels along with 'other room' category
-    "bathroom": ["bathroom"],
-    "bedroom": ["bedroom"],
-    "closet": ["closet"],
-    "dining room": ["dining room"],
-    "garage": ["garage"],
-    "hallway": ["hallway"],
-    "library": ["library"],
-    "laundryroom/mudroom": ["laundry room or mudroom"],  
-    "kitchen": ["kitchen"],
-    "living room": ["living room"],
-    "meetingroom/conferenceroom": ["meeting room or conference room"],
-    "office": ["office"],
-    "porch/terrace/deck/driveway": ["porch or terrace or deck or driveway"],
-    "rec/game": ["recreation or game room"],
-    "stairs": ["stairs"],
-    "utilityroom/toolroom": ["utility room or tool room"],
-    "tv": ["cinema or home theater or theater"],
-    "workout/gym/exercise": ["gym"],
-    "balcony": ["balcony"],
-    "bar": ["bar"],
-    "classroom": ["classroom"],
-    "spa/sauna": ["spa or sauna"],
+    "bathroom": "bathroom",
+    "bedroom": "bedroom",
+    "closet": "closet",
+    "dining room": "dining room",
+    "garage": "garage",
+    "hallway": "hallway",
+    "library": "library",
+    "laundryroom/mudroom": "laundry room or mudroom",  
+    "kitchen": "kitchen",
+    "living room": "living room",
+    "meetingroom/conferenceroom": "meeting room or conference room",
+    "office": "office",
+    "porch/terrace/deck/driveway": "porch or terrace or deck or driveway",
+    "rec/game": "recreation or game room",
+    "stairs": "stairs",
+    "utilityroom/toolroom": "utility room or tool room",
+    "tv": "cinema or home theater or theater",
+    "workout/gym/exercise": "gym",
+    "balcony": "balcony",
+    "bar": "bar",
+    "classroom": "classroom",
+    "spa/sauna": "spa or sauna",
 
 
     ### Following labels are blacklisted but query them in the text prompt anyways
-    "entryway/foyer/lobby": ["entryway or foyer or lobby"], 
-    "outdoor": ["outdoor"],
-    "dining booth": ["dining booth"],
+    "entryway/foyer/lobby": "entryway or foyer or lobby", 
+    "outdoor": "outdoor",
+    "dining booth": "dining booth",
 
-    "other room": ["other room"],
+    "other room": "other room",
 
 
     # Following labels are mapped into less ambiguous labels
